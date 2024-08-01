fd36561c7c2abdd90297f954f20eced9628d81b7
Hyph <79437385+Hyphrio@users.noreply.github.com>
2024-08-01 07:06:10 +0000

    aaand I broke desktop while fixing mobileeee

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,10 +12,12 @@
 
 <slot></slot>
 
-<footer class="bg-gray-3 p-8 prose dark:prose-invert">
-	<p>
-		Made by Hyphr (@Hyphrio) <br />
-		Not affiliated with MC Championship, Noxcrew, Mojang Studios or Microsoft. <br />
-		Player icons from <a href="https://mineatar.io">Mineatar.</a>
-	</p>
+<footer class="bg-gray-3 p-8">
+	<div class="prose dark:prose-invert">
+		<p>
+			Made by Hyphr (@Hyphrio) <br />
+			Not affiliated with MC Championship, Noxcrew, Mojang Studios or Microsoft. <br />
+			Player icons from <a href="https://mineatar.io">Mineatar.</a>
+		</p>
+	</div>
 </footer>
